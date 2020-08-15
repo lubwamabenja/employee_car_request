f2dd9406b39f547d8fd807dea1da84e4e871356d
lubwamabenja <lubwama73@gmail.com>
2020-08-15 13:26:21 +0300

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "employee_car_request",
+
+    'summary': """
+        requests the permission to use the company's cars and define the duration""",
+
+    'description': """
+        HR Employee will create a request and confirm it
+        HR Officer will validate/refuse the request
+        HR Manager will approve the reqest
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
